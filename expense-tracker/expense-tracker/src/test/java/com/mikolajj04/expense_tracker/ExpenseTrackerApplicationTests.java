43b487e33858dc9027d67497711b6fc5bc3901d1
mikolajj04 <Mikolajjussak@gmail.com>
2026-05-19 15:21:00 +0200

    chore: add corretly initialized project structure

---
+++ expense-tracker/expense-tracker/src/test/java/com/mikolajj04/expense_tracker/ExpenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikolajj04.expense_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

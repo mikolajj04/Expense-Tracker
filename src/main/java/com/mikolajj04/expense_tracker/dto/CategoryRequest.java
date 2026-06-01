6fa96c028b6cccc1dd9cd28003295485783a73e4
mikolajj04 <Mikolajjussak@gmail.com>
2026-06-01 19:47:42 +0200

    feat(controller): add CategoryController

--- src/main/java/com/mikolajj04/expense_tracker/dto/CategoryRequest.java
+++ src/main/java/com/mikolajj04/expense_tracker/dto/CategoryRequest.java
@@ -1,5 +1,6 @@
 package com.mikolajj04.expense_tracker.dto;
 import lombok.Data;
+@Data
 public class CategoryRequest {
     private String name;
     private String description;
--- src/main/java/com/mikolajj04/expense_tracker/dto/CategoryResponse.java
+++ src/main/java/com/mikolajj04/expense_tracker/dto/CategoryResponse.java
@@ -1,6 +1,6 @@
 package com.mikolajj04.expense_tracker.dto;
 import lombok.Data;
-
+@Data
 public class CategoryResponse {
     private Long id;
     private String name;

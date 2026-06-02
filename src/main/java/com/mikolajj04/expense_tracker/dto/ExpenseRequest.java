deacb2ebc15d2dd7f1b989ec841a6e826eb92a78
mikolajj04 <Mikolajjussak@gmail.com>
2026-06-02 11:06:04 +0200

    feat(controller): add ExpenseController

--- src/main/java/com/mikolajj04/expense_tracker/dto/ExpenseRequest.java
+++ src/main/java/com/mikolajj04/expense_tracker/dto/ExpenseRequest.java
@@ -2,7 +2,7 @@ package com.mikolajj04.expense_tracker.dto;
 import lombok.Data;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-
+@Data
 public class ExpenseRequest {
     private BigDecimal amount;
     private String description;
--- src/main/java/com/mikolajj04/expense_tracker/dto/ExpenseResponse.java
+++ src/main/java/com/mikolajj04/expense_tracker/dto/ExpenseResponse.java
@@ -1,5 +1,4 @@
 package com.mikolajj04.expense_tracker.dto;
-
 import lombok.Data;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
